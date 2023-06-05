118796ebd805bfefe6eda1c2a64dbeb4f7e50526
oi <alysson.cordeiro@sou.inteli.edu.br>
2023-06-04 23:24:44 -0300

    feat(alteração no input): alteração

--- src/app/renderer/pages/components/InputMap.js
+++ src/app/renderer/pages/components/InputMap.js
@@ -33,46 +33,49 @@ class Component extends React.Component {
             >
                 <Grid container spacing={1}>
 
-                <Grid item xs={12}>
-                    <InputLabel sx={{color: '#fff', marginLeft: '3px', marginY: '7px'}}>Mapa: {this.map_name}</InputLabel>
-                </Grid>
+                    <Grid item xs={12}>
+                        <InputLabel sx={{color: '#fff', marginLeft: '3px', marginY: '7px'}}>Mapa: {this.map_name}</InputLabel>
+                    </Grid>
                 
                
-                <Grid item xs={'auto'}>
-                    <InputLabel sx={{color: '#fff', marginLeft: '3px'}}>Dimensões: </InputLabel>
-                    <InputLabel sx={{color: '#fff', marginLeft: '3px'}}>Altura: {this.height}</InputLabel>
-                    <InputLabel sx={{color: '#fff', marginLeft: '3px'}}>Largura: {this.width}</InputLabel>
-                </Grid>
+                    <Grid item xs={'auto'}>
+                        <InputLabel sx={{color: '#fff', marginLeft: '3px'}}>Dimensões: </InputLabel>
+                        <InputLabel sx={{color: '#fff', marginLeft: '3px'}}>Altura: {this.height}</InputLabel>
+                        <InputLabel sx={{color: '#fff', marginLeft: '3px'}}>Largura: {this.width}</InputLabel>
+                    </Grid>
     
 
-                <Grid item xs={9} sx={{ textAlign: 'center', display: 'flex', alignItems: 'center' }}>
-                    <img src='' alt='' />
-                    <InputLabel sx={{color: '#fff', alignContent: 'center', marginLeft: '3px' }}>N° de divisões:</InputLabel>
+                    <Grid item xs={9} sx={{ textAlign: 'center', display: 'flex', alignItems: 'center' }}>
+                        
+                        <InputLabel sx={{color: '#fff', alignContent: 'center', marginLeft: '3px' }}>N° de divisões:</InputLabel>
 
-                    <Grid item xs={2}>
-                    <TextField variant="standard" sx={{backgroundColor: 'primary.dark', marginLeft: '7px', color: '#fff'}}/>
+                        <Grid item xs={2}>
+                        <TextField variant="standard" sx={{backgroundColor: 'primary.dark', marginLeft: '7px', color: '#fff'}}/>
+                        </Grid>
+                        
                     </Grid>
 
-                </Grid>
-
                
-                <Grid container spacing={1}></Grid>
+                    <Grid container spacing={1}></Grid>
 
-                <Grid item xs={3}>
-                    <img src='C:\Users\filip\OneDrive\Documentos\Inteli\Grupo05\media\delivery.png' alt='Robot icon' />
-                </Grid>
+                    <Grid item xs={3}>
+                    <img src="../../../images/turtlebot3-simulation3.png" alt="Robot"/>
+                    </Grid>
+
                 
-                <Grid item xs={3}>
-                    <TextField id="x-origin" label="X" variant="outlined" sx={{backgroundColor: '#fff'}} size='small'/>
-                    <TextField id="y-origin" label="Y" variant="outlined" sx={{backgroundColor: '#fff'}} size='small'/>
-                </Grid>
-                <Grid item xs={3}>
-                    <img src='C:\Users\filip\OneDrive\Documentos\Inteli\Grupo05\media\delivery.png' alt='Target icon' />
-                </Grid>
-                <Grid item xs={3}>
-                    <TextField id="x-destiny" label="X" variant="outlined" sx={{backgroundColor: '#fff'}} size='small'/>
-                    <TextField id="y-destiny" label="Y" variant="outlined" sx={{backgroundColor: '#fff'}} size='small'/>
-                </Grid>
+                    <Grid item xs={3}>
+                        <TextField id="x-origin" label="X" variant="outlined"  sx={{backgroundColor: '#fff', borderRadius: '7px', marginTop: '12px', marginRight: '5px'}} size='small'/>
+                        <TextField id="y-origin" label="Y" variant="outlined" sx={{backgroundColor: '#fff', borderRadius: '7px', marginTop: '13px', marginRight: '5px'}} size='small'/>
+                    </Grid>
+
+                    <Grid item xs={3}>
+                       <img src='C:\Users\filip\OneDrive\Documentos\Inteli\Grupo05\media\delivery.png' alt='Target icon' />
+                    </Grid>
+
+                    <Grid item xs={3}>
+                        <TextField id="x-destiny" label="X" variant="outlined" sx={{backgroundColor: '#fff', borderRadius: '7px', marginTop: '12px', marginRight: '5px'}} size='small'/>
+                        <TextField id="y-destiny" label="Y" variant="outlined" sx={{backgroundColor: '#fff', borderRadius: '7px', marginTop: '13px', marginRight: '5px'}} size='small'/>
+                    </Grid>
                 </Grid>
             </Box>
         );
